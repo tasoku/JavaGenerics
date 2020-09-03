12d41a1267a8339428dadef0872599c7041c9e62
TARUN KUMAR <kumar.tarun202@gmail.com>
2020-09-03 11:25:05 +0530

    First commit

---
+++ generics/src/main/java/com/generics/generics/App.java
@@ -0,0 +1,13 @@
+package com.generics.generics;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
